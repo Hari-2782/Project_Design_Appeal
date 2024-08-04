b410d73e21094746efbcf8821157be73a485133c
Piranavan2000 <sumathysathiyavannan@gmail.com>
2024-08-04 22:46:39 +0530

    feed

--- frontend/src/components/Homepage/HomeEnq.jsx
+++ frontend/src/components/Homepage/HomeEnq.jsx
@@ -3,8 +3,15 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from './Typography';
+import { useNavigate } from 'react-router-dom';
 
 function ProductSmokingHero() {
+  const navigate = useNavigate(); // Get the navigate function from useNavigate
+
+  const handleRedirect = () => {
+    navigate('/feedback'); // Use navigate to redirect to the feedback page
+  };
+
   return (
     <Container
       component="section"
@@ -17,19 +24,21 @@ function ProductSmokingHero() {
           height: 'auto',
           py: 2,
           px: 5,
-          
-        }}>
+        }}
+        onClick={handleRedirect} // Attach the handleRedirect function to onClick
+      >
         <Typography variant="h4" component="span">
           Got any questions? Need help?
         </Typography>
       </Button>
+
       <Typography variant="subtitle1" sx={{ my: 3 }}>
         We are here to help. Get in touch!
       </Typography>
       <Box
         component="img"
         src="/contuct.png"
-        alt="Contuct"
+        alt="Contact"
         sx={{ width: 60 }}
       />
     </Container>
